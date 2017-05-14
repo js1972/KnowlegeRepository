0c9f2ad62d263fc2c5c5089c566f73cad6625e55
jerry <cle.ee@163.com>
2017-05-14 12:06:17 +0200

    start first step

---
+++ practice/corresponding/CL_JS_CORRESPONDING.js
@@ -0,0 +1,14 @@
+var CL_JS_CORRESPONDING = (function(){
+  var _bind = function(obj){
+   
+  };
+  var _getValue = function(obj){
+    
+  };
+  var handler = function(){};
+
+  handler.prototype.CREATE = function(src,target,mapping){
+   	  console.log("Hello world"); 
+  };
+  return new handler;
+})();
---
+++ practice/corresponding/developer.js
@@ -0,0 +1,9 @@
+function Developer(name, language, salary){
+	this.name = name;
+	this.focusLanguage = language;
+	this.salary = salary;
+}
+
+function Presales(name){
+	this.name = name;
+}
