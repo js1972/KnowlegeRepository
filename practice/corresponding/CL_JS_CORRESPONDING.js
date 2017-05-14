290da1a5fecf6eb6198fa06e09c19850e920d379
jerry <cle.ee@163.com>
2017-05-14 13:34:06 +0200

    almost done

--- practice/corresponding/CL_JS_CORRESPONDING.js
+++ practice/corresponding/CL_JS_CORRESPONDING.js
@@ -1,14 +1,75 @@
+var CL_JS_CORRESPONDING = function() { 
+  
+ function MappingExecutor(src, target, mapping){
+ 	this.src = src;
+ 	this.target = target;
+ 	this.mapping = mapping;
+
+ 	function _map(source, target, mapping){
+ 		for( var i = 0; i < source.length; i++){
+ 			_mapEach(source[i], target[i], mapping);
+ 		}
+ 	}
+
+ 	function _mapEach(source, target, mapping){
+ 		target[mapping.target] = source[mapping.source];
+ 	}
+ 	MappingExecutor.prototype.execute = function(){
+ 		for( var i = 0; i < this.mapping.length; i++){
+ 			_map(this.src, this.target, this.mapping[i]);
+ 		}
+ 		return this.target;
+ 	}
+ };
+
+ return { 
+    CREATE:function(src,target,mapping){
+   	  if( !Array.isArray(src) || !Array.isArray(target)){
+   	  	return target;
+   	  }
+   	  if( src.length != target.length){
+   	  	return target;
+   	  }
+   	  if( src.length == 0){
+   	  	return target;
+   	  }
+   	  return new MappingExecutor(src, target, mapping);
+  }
+}}(); 
+
+/*
 var CL_JS_CORRESPONDING = (function(){
-  var _bind = function(obj){
-   
+  var _map = function(){
+   	for( var i = 0; i < this.mappingRule.length; i++){
+   		_mapEach(this.source1, this.target, this.mappingRule[i]);
+   	}
   };
-  var _getValue = function(obj){
-    
+  var _mapEach = function(source, target, mappingRule){
+     for( var i = 0; i < source.length; i++){
+     	target[i][mappingRule.target] = source[i][mappingRule.source];
+     }
   };
   var handler = function(){};
 
   handler.prototype.CREATE = function(src,target,mapping){
-   	  console.log("Hello world"); 
+   	  if( !Array.isArray(src) || !Array.isArray(target)){
+   	  	return target;
+   	  }
+   	  if( src.length != target.length){
+   	  	return target;
+   	  }
+   	  if( src.length == 0){
+   	  	return target;
+   	  }
+   	  this.sourceObject = src;
+   	  this.targetObject = target;
+   	  this.mappingRule = mapping;
+   	  return this;
+  };
+
+  handler.prototype.EXECUTE = function(){
+  	_map();
+  	return this.target;
   };
   return new handler;
-})();
+})();*/
