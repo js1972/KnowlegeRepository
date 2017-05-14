8b5adb4d5dd866e99f87b5cf295f4d4611400fd5
jerry <cle.ee@163.com>
2017-05-14 14:13:24 +0200

    source codre format

--- practice/corresponding/CL_JS_CORRESPONDING.js
+++ practice/corresponding/CL_JS_CORRESPONDING.js
@@ -4,13 +4,11 @@ var CL_JS_CORRESPONDING = function() {
  	this.src = src;
  	this.target = target;
  	this.mapping = mapping;
-
  	function _map(source, target, mapping){
  		for( var i = 0; i < source.length; i++){
  			_mapEach(source[i], target[i], mapping);
  		}
  	}
-
  	function _mapEach(source, target, mapping){
  		target[mapping.target] = source[mapping.source];
  		if( mapping.function){
@@ -24,7 +22,6 @@ var CL_JS_CORRESPONDING = function() {
  		return this.target;
  	}
  };
-
  return { 
     CREATE:function(src,target,mapping){
    	  if( !Array.isArray(src) || !Array.isArray(target)){
@@ -39,40 +36,3 @@ var CL_JS_CORRESPONDING = function() {
    	  return new MappingExecutor(src, target, mapping);
   }
 }}(); 
-
-/*
-var CL_JS_CORRESPONDING = (function(){
-  var _map = function(){
-   	for( var i = 0; i < this.mappingRule.length; i++){
-   		_mapEach(this.source1, this.target, this.mappingRule[i]);
-   	}
-  };
-  var _mapEach = function(source, target, mappingRule){
-     for( var i = 0; i < source.length; i++){
-     	target[i][mappingRule.target] = source[i][mappingRule.source];
-     }
-  };
-  var handler = function(){};
-
-  handler.prototype.CREATE = function(src,target,mapping){
-   	  if( !Array.isArray(src) || !Array.isArray(target)){
-   	  	return target;
-   	  }
-   	  if( src.length != target.length){
-   	  	return target;
-   	  }
-   	  if( src.length == 0){
-   	  	return target;
-   	  }
-   	  this.sourceObject = src;
-   	  this.targetObject = target;
-   	  this.mappingRule = mapping;
-   	  return this;
-  };
-
-  handler.prototype.EXECUTE = function(){
-  	_map();
-  	return this.target;
-  };
-  return new handler;
-})();*/
