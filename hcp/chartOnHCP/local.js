032735448a0df0afe4a49b59a221003357f55dd7
i042416 <cle.ee@163.com>
2017-05-31 16:57:31 +0200

    static html file can work now

--- hcp/chartOnHCP/local.js
+++ hcp/chartOnHCP/local.js
@@ -1,6 +1,8 @@
 var path = require('path'), express = require('express');
 var app = express();
+
 app.use('/jerry', express.static(path.join(__dirname, 'webapp')));
+
 app.get('/', function(req, res){
    res.send("Hello World");
 });
--- practice/nodejs/jsonpServer.js
+++ practice/nodejs/jsonpServer.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();  
 const port = 3000;
 
+var path = require('path');
+
+// The skeleton for this applies an express.static method as 
+// Express middleware via app.use, looking like this.
+// app.use(express.static());
+// app.use('/jerry', express.static(path.join(__dirname, 'webapp')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
@@ -24,3 +31,15 @@ app.listen(port, (err) => {
 
   console.log(`server is listening on ${port}`)
 });
+
+/* Jerry 2017-05-31 16:52PM 
+Jerry alternative:
+// Define the port to run on
+app.set('port', 3000);
+// Listen for requests
+var server = app.listen(app.get('port'), function() {
+  var port = server.address().port;
+  console.log('Magic happens on port ' + port);
+});
+
+*/
