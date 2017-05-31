f5045a0ddc5ea0fc437653666293d89d318816b4
i042416 <cle.ee@163.com>
2017-05-31 11:20:58 +0200

    first upload

---
+++ practice/nodejs/index.js
@@ -0,0 +1,18 @@
+const http = require('http')  
+const port = 3000
+
+
+const requestHandler = (request, response) => {  
+  console.log(request.url) // now output to CMD console, not browser any more
+  response.end('Hello Node.js Server!')
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {  
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
