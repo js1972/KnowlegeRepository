9c5a2d94d3ef60d7b4506ad7c3a571f31791e235
i042416 <cle.ee@163.com>
2017-06-03 22:08:30 +0200

    can work

--- practice/nodejs/Server3000.js
+++ practice/nodejs/Server3000.js
@@ -3,14 +3,19 @@ const app = express();
 const port = 3000;
 
 var path = require('path');
-
+var repo = {
+	"I042416": "Jerry",
+	"I042417": "Tom",
+	"I042418": "Jim"
+}
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/request', (request, response) => {  
-  console.log(request.headers);
+  // console.log(request.headers);
+  console.log(request.query.id);
   response.json({
-    chance: "Hello " + port
-  })
+    UserName: repo[request.query.id]
+  });
 });
 
 app.get('/', (request, response) => {  
@@ -20,6 +25,5 @@ app.listen(port, (err) => {
   if (err) {
     return console.log('something bad happened', err)
   }
-
   console.log(`server is listening on ${port}`)
 });
